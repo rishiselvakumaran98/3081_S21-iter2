344fed93b2617d32fc64040d11820058fb3c6e13
Aaron Koenigsberg <koeni296@umn.edu>
2021-01-21 15:12:05 -0600

    Removing project-related files and adding lab

--- project/bin/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ROOTDIR=`git rev-parse --show-toplevel`
-
-${ROOTDIR}-build/test/csci3081tests
--- project/src/config/project_settings.cc
+++
@@ -1,9 +0,0 @@
-#include <EntityProject/project_settings.h>
-#include <string>
-#include "../delivery_simulation.h"
-
-extern "C" {
-  entity_project::IEntitySystem* GetEntitySystem(const std::string& id) {
-        return new csci3081::DeliverySimulation();
-  }
-}
